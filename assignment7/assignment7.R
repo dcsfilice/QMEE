8ad80401306ef07c04e6d28f04a4f35bd47a77a6
David Filice <filicd1@mcmaster.ca>
2019-03-01 09:15:18 -0500

    update

--- assignment7/assignment7.R
+++ assignment7/assignment7.R
@@ -1,7 +1,9 @@
 library(R2jags)
 dat<-read.csv("remating.csv")
-model<-lm(duration~treatment*line,data=dat)
-summary(model)
+#Create linear model of data: Mating duration as response, with experience treatment and genetic line as predictors
+model<-lm(first.d~treatment*line,data=dat)
+#run JAGS
+#ma=experience treatment, mb=genetic line, mab=interaction between the two
 N <- nrow(dat)
 bayesmod <- with(dat, jags(model.file='bayes.bug'
                           , parameters=c("ma", "mb", "mab","tau")
@@ -9,9 +11,16 @@ bayesmod <- with(dat, jags(model.file='bayes.bug'
                           , n.chains = 4
                           , inits=NULL
 ))
-
+#Let's look at
 print(bayesmod)
-plot(bayesmod)
+#Based on the 95% confidence intervals, the variances for line and treatment alone are signifcant as they do not overlap zero. The interaction term does.
 
-
+#plot(bayesmod)
+#Display SD of each predictor
+#traceplot(bayesmod)
+#View traceplots - everything looks okay.
+#Now, let's take a frequenist approach to analyzing our model
+library(car)
+Anova(model)
+#Interesting, similar to our bayes model, we observe p-values < 0.5 for treatment and line alone, but not for their interaction
 
