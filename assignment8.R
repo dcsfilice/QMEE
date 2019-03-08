3778b97e286f9edc12cda4cea399bcca1e0dd0ab
David Filice <filicd1@mcmaster.ca>
2019-03-08 13:15:15 -0500

    a8

--- assignment8.R
+++ assignment8.R
@@ -1,5 +1,6 @@
 dat<-read.csv("remating.csv")
 #Let's look if the previous competitive experience of a male influences the remating propensity of his mate
+#binary response (0,1), lets use binomial family
 model<-glm(mating~treatment,family=binomial,data=dat)
 library(car)
 Anova(model)
