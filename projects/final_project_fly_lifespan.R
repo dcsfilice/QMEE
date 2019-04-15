0bfb1a4f3f4acbe9fe7d82e68c009f858de2ccad
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-04-15 10:52:03 -0400

    Yeah - susan's file finally :-)

--- projects/final_project_fly_lifespan.R
+++ projects/final_project_fly_lifespan.R
@@ -65,7 +65,6 @@ plot(allEffects(fly_dat1.lmer))
 plot(allEffects(model1b))
 
 
-
 ####### NOW start working with log transformed data (female lifespan)
 
 fly_dat2 <- fly_dat1 %>%  
@@ -96,13 +95,15 @@ plot(allEffects(fly_dat2.lm))
 
 
 ## Design the mixed model to include these parameters:
-# FIXED effect: female line (factor with 2 levels)   NOT ADDED YET!!
+# FIXED effect: female line (factor with 2 levels)
 # RANDOM effect: male clonal backgrounds (factor with 6 levels)
+
+# these are both the same but one for DF name conventions and one for me :-)
 fly_dat2.lmer <-  lmer(log_lifespan~treatment + (1|line), data=fly_dat2)
-# This is not working yet?:
 model2b <- lmer(log_lifespan ~ treatment * pop + (1|line), data=fly_dat2)
 
-# Check summary stats for the model
+
+# Check summary stats for the model (Again these are the same)
 summary(fly_dat2.lmer)  
 summary(model2b)
 
@@ -115,6 +116,99 @@ summary(model2b)
 plot(allEffects(fly_dat2.lmer))
 plot(allEffects(model2b))
 
+# Use a histogram to look at distribution of lifespan vs log_lifespan
+ggplot(data=fly_dat1, aes(lifespan)) + 
+  geom_histogram()
+
+ggplot(data=fly_dat2, aes(log_lifespan)) + 
+  geom_histogram()
+
+# Use Shapiro-Wilk test to test normality of lifespan vs log_lifespan
+# If p>0.5 then data is normally distributed
+shapiro.test(fly_dat1$lifespan)
+shapiro.test(fly_dat2$log_lifespan)
 
 
 
+## *********************************
+# Here goes for the Box-Cox transformation  :-O
+# http://rcompanion.org/handbook/I_12.html
+
+library(MASS)
+
+Box = boxcox(lifespan ~ pop,
+             data = fly_dat1,
+             lambda = seq(-6,6,0.1)
+)
+
+Cox = data.frame(Box$x, Box$y)
+
+Cox2 = Cox[with(Cox, order(-Cox$Box.y)),]
+
+Cox2[1,]
+
+lambda = Cox2[1, "Box.x"]
+
+fly_dat1$lifespan_box = (fly_dat1$lifespan ^ lambda - 1)/lambda   
+
+boxplot(lifespan_box ~ pop,
+        data = fly_dat1,
+        ylab="Box–Cox-transformed lifespan",
+        xlab="Population")
+
+
+
+##Perform ANOVA and check residuals
+
+model = lm(lifespan_box ~ pop, 
+           data=fly_dat1)
+
+library(car)
+
+Anova(model, type="II")
+                
+x = residuals(model)
+
+library(rcompanion)
+plotNormalHistogram(x)
+
+
+###### This is useful for printing ggplots to our folder
+
+# EXAMPLES
+#p1 <- ggplot( data = df1, aes(x = Date,y = Avg.Pace, color = Distance)) + 
+  geom_point() +
+  scale_y_datetime(date_labels = "%M:%S") +
+  geom_smooth(color = "orange") +
+  labs(x = "Date", y = "Average Pace (min/km)")
+
+  # save all plots  EXAMPLES
+#  ggsave("allPace.png", plot = p1, width = 8, height = 4, dpi = "print")
+
+######  sum-to-zero contrasts :-)
+#  http://atyre2.github.io/2016/09/03/sum-to-zero-contrasts.html  
+  
+  
+## STEPS  
+# plot the data to find the intercept
+# center the continuous variable
+# replot the data  
+# use sum to zero contrast for female population
+
+  # plot the data to find the intercept  
+  m0 <- lm(lifespan ~ treatment * pop, data = fly_dat1)
+  (summary_m0 <- summary(m0)) 
+
+#####  LEFT OFF HERE!!!  Still working on this 
+  # center the continuous variable
+  base_lifespan <- ggplot(fly_dat1, aes(x = lifespan, y = treatment)) + geom_point(aes(shape = Species)) + 
+    xlab("Sepal Length [mm]") + ylab("Sepal Width [mm]")
+  library(broom)
+  nd <- expand.grid(Sepal.Length = seq(-1, 8, 0.1), Species = factor(levels(iris$Species)))
+  pred.0 <- augment(m0, newdata = nd)
+  base_iris + geom_line(aes(y = .fitted, linetype = Species), data = pred.0)    
+  
+  library(dplyr)  #Stay in the tidyverse! 
+  iris <- iris %>% mutate(cSepal.Length = Sepal.Length - mean(Sepal.Length))
+  m1 <- lm(Sepal.Width ~ cSepal.Length * Species, data = iris)
+  (summary_m1 <- summary(m1))
