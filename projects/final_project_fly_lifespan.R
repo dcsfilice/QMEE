021ce9bc1154378b75d3e38d5f4ea2dcfb44e2d9
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-04-15 10:51:42 -0400

    yeah - here is susan's file in progress :-)

--- projects/final_project_fly_lifespan.R
+++ projects/final_project_fly_lifespan.R
@@ -1,4 +1,5 @@
-### Final QMEE project
+### Final QMEE project with David :-)
+
 ## Question 1.  
 # HYPOTHESIS: Males previously housed with rival males will reduce the lifespan 
 # of their female mates
@@ -17,7 +18,7 @@ fly_dat1 <- read.csv("lifespan.csv")
 # Check that data was read in properly
 summary(fly_dat1)
 str(fly_dat1)
-# No cleaning needed
+# No cleaning needed :-)
 
 
 ## Compare lifespan between females 
@@ -45,24 +46,74 @@ plot(allEffects(fly_dat1.lm))
 
 
 ## Design the mixed model to include these parameters:
-# FIXED effect: female line (factor with 2 levels)   NOT ADDED YET!!
+# FIXED effect: female line (factor with 2 levels) 
 # RANDOM effect: male clonal backgrounds (factor with 6 levels)
-fly_dat2.lm <-  lmer(lifespan~treatment + (1|line), data=fly_dat1)
+fly_dat1.lmer <-  lmer(lifespan~treatment + (1|line), data=fly_dat1)
+model1b <- lmer(lifespan ~ treatment * pop + (1|line), data=fly_dat1)
 
 
 # Check summary stats for the model
-summary(fly_dat2.lm)  
+summary(fly_dat1.lmer)  
+summary(model1b)
 # Var and Std Dev is low/high for Random Effects?
 # Therefore random effect of ? has ? impact
 # on overall model?
 
 
 # How does effects plot changes after running mixed model
+plot(allEffects(fly_dat1.lmer))
+plot(allEffects(model1b))
+
+
+
+####### NOW start working with log transformed data (female lifespan)
+
+fly_dat2 <- fly_dat1 %>%  
+  mutate(log_lifespan = log(lifespan))
+
+## Compare lifespan between females 
+# TREATMENT mated with males housed either SINGLE or with RIVALS
+# RESPONSE lifespan of individual female by treatment
+
+fly_dat2.lm <- lm(log_lifespan~treatment, data = fly_dat2)
+summary(fly_dat2.lm)
+
+
+# Check diagnostic plots
+par(mfrow=c(2,2))  # show four graphs in one panel
+plot(lm(fly_dat2.lm), las = 1, col = "red")  # no issues in these plots?
+
+
+# Look at the the data 
+print(ggplot(fly_dat2.lm, aes(x=treatment, y=log_lifespan))
+      + geom_boxplot()
+)
+
+
+# Plot an effects plot 
+library(effects)
 plot(allEffects(fly_dat2.lm))
 
 
+## Design the mixed model to include these parameters:
+# FIXED effect: female line (factor with 2 levels)   NOT ADDED YET!!
+# RANDOM effect: male clonal backgrounds (factor with 6 levels)
+fly_dat2.lmer <-  lmer(log_lifespan~treatment + (1|line), data=fly_dat2)
+# This is not working yet?:
+model2b <- lmer(log_lifespan ~ treatment * pop + (1|line), data=fly_dat2)
+
+# Check summary stats for the model
+summary(fly_dat2.lmer)  
+summary(model2b)
+
+# Var and Std Dev is low/high for Random Effects?
+# Therefore random effect of ? has ? impact
+# on overall model?
 
 
+# How does effects plot changes after running mixed model
+plot(allEffects(fly_dat2.lmer))
+plot(allEffects(model2b))
 
 
 
