982f6bd5c3df3f080661db0c5c67d4d837221f39
David Filice <filicd1@mcmaster.ca>
2019-04-21 09:59:10 -0400

    u

--- projects/final_project_fly_offspring.R
+++ projects/final_project_fly_offspring.R
@@ -4,18 +4,15 @@ library(car)
 dat1 <- read.csv("offspring.num.csv")
 dat2 <- read.csv("lifespan.csv")
 
-#maximal model, singular fit
-model1a<-lmer(lifespan~treatment*pop+(treatment*pop|line),data=dat2)
-#simplified model
-model1b<-lmer(lifespan~treatment*pop+(1|line),data=dat2)
-summary(model1b)
 model1c<-coxme(Surv(lifespan) ~ treatment*pop + (1|line) , data=dat2)  
 Anova(model1c)
 summary(model1c)  
 
-#maximal model, singular fit
+#maximal model, count data, let's try a poisson distribution
 model2a<-glmer(offspring~treatment*day*pop+(treatment*pop|line)+((poly(day,2))-1|line)+(poly(day,2)|female),data=dat1,family=quasipoisson)
+#singular fit
 
+#We reduced our random effect interactions until no singular fits
 model2b<-glmer(offspring~treatment*day*pop+(1|line)+(1|female),data=dat1,family=poisson)
 #singular fit, but we will bear with it since it is from female identity random factor. We need this here to account for repeated measures.
 
@@ -31,16 +28,11 @@ overdisp_fun <- function(model) {
 overdisp_fun(model2b)
 #The model is very overdispersed, likely because of many zero counts in dataset.
 #Let's use a negative binomial family instead
-model2c<-glmer.nb(offspring~treatment*day*pop+(1|line)+(1|female),data=dat1,family=nbinom)
+library(glmmTMB)
+model2c<- model2c<-glmmTMB(offspring~treatment*day*pop+(1|line)+(1|female),data=dat1,family=nbinom2)
 #Singular fit, but let's bear with it considering it is of course from the OBS random factor (and female factor)
 overdisp_fun(model2c)
 #Model no longer looks overdispersed! 
 summary(model2c)
-
-confint(model2c,method="wald")
-
-library(glmmTMB)
-library(glmmTMB)
-> model2c<-glmmTMB(offspring~treatment*day*pop+(1|line)+(1|female),data=dat1,family=nbinom2)
 
 confint(model2c,method="uniroot")
