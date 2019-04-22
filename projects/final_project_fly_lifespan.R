b393c742329950b4161b6ed131c9c8b613fb17a8
Susan Marsh-Rollo <marshse@mcmaster.ca>
2019-04-22 12:55:28 -0400

    Final edits to file SMR DF

--- projects/final_project_fly_lifespan.R
+++ projects/final_project_fly_lifespan.R
@@ -46,13 +46,6 @@ lambda = Cox2[1, "Box.x"]
 
 fly_dat1$sqrt_lifespan = sqrt(fly_dat1$lifespan)
 
-##Perform ANOVA on Box.Cox transformed liFEspan and check residuals
-model = lm(lifespan_box ~ pop, 
-           data=fly_dat1)
-library(car)
-Anova(model, type="II")
-x = residuals(model)
-
 
 ## HISTOGRAMS OF FEMALE LIFESPAN (3 THREE VERSIONS)
 
@@ -71,12 +64,11 @@ h2 <- ggplot(data=fly_dat1, aes(log_lifespan)) +
 ggsave("histogram_female-log_lifespan.png", plot = h2, width = 8, height = 4, dpi = "print")
 
 # Square Root transformed    
-#library(rcompanion)
-#plotNormalHistogram(x)
 h3 <- ggplot(data=fly_dat1, aes(sqrt_lifespan)) + 
   ggtitle("Female Lifespan, square root transformed")+ 
   geom_histogram()
 ggsave("histogram_female-sqrt_lifespan.png", plot = h3, width = 8, height = 4, dpi = "print")
+
 
 ## NORMALITY TEST
 
@@ -110,7 +102,7 @@ ggsave("diagnostic_female-log_lifespan.png", plot = diag2, width = 8, height = 4
 # Square Root transformed female lifespan
 fly_dat3.lm <- lmer(sqrt_lifespan~treatment*pop+(1|line), data = fly_dat1)
 diagnos3 <- plot(lm(fly_dat3.lm), las = 1, col = "green")  # no issues in these plots?
-ggsave("diagnostic_female-Box.Cox_lifespan.png", plot = diag3, width = 8, height = 4, dpi = "print")
+ggsave("diagnostic_female-square_root_lifespan.png", plot = diag3, width = 8, height = 4, dpi = "print")
 
 # Based on our various diagnostics, it appears lifespan square-root transformed is the best fit
 # Looking at residuals:
@@ -150,146 +142,6 @@ survmodel<-coxme(Surv(lifespan) ~ treatment*pop + (1|line) , data=fly_dat1)
 summary(survmodel)
 
 
-# What happens if try a sum-to-zero contrast for female lifespan?
-
-# Look at the original model  
-m_original <- lm(lifespan ~ treatment * pop, data = fly_dat1)
-(summary_m_original <- summary(m_original)) 
-
-# center the continuous variable
-library(dplyr) 
-fly_dat1 <- fly_dat1 %>% mutate(clifespan = lifespan - mean(lifespan))
-m_centered <- lm(clifespan ~ treatment * pop, data = fly_dat1)
-(summary_m_centered <- summary(m_centered))
+# End script
 
 
-# Compare the original model with a model containing centred female lifespan
-anova(m_original)
-anova(m_centered)
-
-# Make some comments here about the comparison
-# Should we use the full mixed effects model here?
-
-
-
-
-#####  LEFT OFF HERE!!!  Still working on this Apr 21, 2019
-###**********  EVERYTHING BELOW THIS LINE IS THE ORIGINAL SCRIPT WE WERE WORKING WITH
-
-## Compare lifespan between females 
-# TREATMENT mated with males housed either SINGLE or with RIVALS
-# RESPONSE lifespan of individual female by treatment
-
-fly_dat1.lm <- lm(lifespan~treatment, data = fly_dat1)
-summary(fly_dat1.lm)
-
-
-# Check diagnostic plots
-par(mfrow=c(2,2))  # show four graphs in one panel
-plot(lm(fly_dat1.lm), las = 1, col = "purple")  # no issues in these plots?
-
-# Look at the the data 
-print(ggplot(fly_dat1.lm, aes(x=treatment, y=lifespan))
-      + geom_boxplot()
-)
-
-# Plot an effects plot 
-library(effects)
-plot(allEffects(fly_dat1.lm))
-
-## Design the mixed model to include these parameters:
-# FIXED effect: female line (factor with 2 levels) 
-# RANDOM effect: male clonal backgrounds (factor with 6 levels)
-fly_dat1.lmer <-  lmer(lifespan~treatment + (1|line), data=fly_dat1)
-model1b <- lmer(lifespan ~ treatment * pop + (1|line), data=fly_dat1)
-
-# Check summary stats for the model
-summary(fly_dat1.lmer)  
-summary(model1b)
-# Var and Std Dev is low/high for Random Effects?
-# Therefore random effect of ? has ? impact
-# on overall model?
-
-# How does effects plot changes after running mixed model
-plot(allEffects(fly_dat1.lmer))
-plot(allEffects(model1b))
-
-# Plot an effects plot 
-library(effects)
-plot(allEffects(fly_dat2.lm))
-
-## Design the mixed model to include these parameters:
-# FIXED effect: female line (factor with 2 levels)
-# RANDOM effect: male clonal backgrounds (factor with 6 levels)
-
-# these are both the same but one for DF name conventions and one for me :-)
-fly_dat2.lmer <-  lmer(log_lifespan~treatment + (1|line), data=fly_dat2)
-model2b <- lmer(log_lifespan ~ treatment * pop + (1|line), data=fly_dat2)
-
-# Check summary stats for the model (Again these are the same)
-summary(fly_dat2.lmer)  
-summary(model2b)
-
-# Var and Std Dev is low/high for Random Effects?
-# Therefore random effect of ? has ? impact
-# on overall model?
-
-# How does effects plot changes after running mixed model
-plot(allEffects(fly_dat2.lmer))
-plot(allEffects(model1b))
-
-# Use a histogram to look at distribution of lifespan vs log_lifespan
-ggplot(data=fly_dat1, aes(lifespan)) + 
-  geom_histogram()
-
-ggplot(data=fly_dat2, aes(log_lifespan)) + 
-  geom_histogram()
-
-###### This is useful for printing ggplots to our folder
-
-# EXAMPLES
-#p1 <- ggplot( data = df1, aes(x = Date,y = Avg.Pace, color = Distance)) + 
-  geom_point() +
-  scale_y_datetime(date_labels = "%M:%S") +
-  geom_smooth(color = "orange") +
-  labs(x = "Date", y = "Average Pace (min/km)")
-
-  # save all plots  EXAMPLES
-#  ggsave("allPace.png", plot = p1, width = 8, height = 4, dpi = "print")
-
-######  sum-to-zero contrasts :-)
-#  http://atyre2.github.io/2016/09/03/sum-to-zero-contrasts.html  
-  
-## STEPS  
-# plot the data to find the intercept
-# center the continuous variable
-# replot the data  
-# use sum to zero contrast for female population
-
-  # plot the data to find the intercept  
-  m0 <- lm(lifespan ~ treatment * pop, data = fly_dat1)
-  (summary_m0 <- summary(m0)) 
-
-#####  LEFT OFF HERE!!!  Still working on this 
-  # center the continuous variable
-  base_lifespan <- ggplot(fly_dat1, aes(x = lifespan, y = treatment)) + geom_point(aes(shape = Species)) + 
-    xlab("Sepal Length [mm]") + ylab("Sepal Width [mm]")
-  library(broom)
-  nd <- expand.grid(Sepal.Length = seq(-1, 8, 0.1), Species = factor(levels(iris$Species)))
-  pred.0 <- augment(m0, newdata = nd)
-  base_iris + geom_line(aes(y = .fitted, linetype = Species), data = pred.0)    
-  
-  library(dplyr)  #Stay in the tidyverse! 
-  iris <- iris %>% mutate(cSepal.Length = Sepal.Length - mean(Sepal.Length))
-  m1 <- lm(Sepal.Width ~ cSepal.Length * Species, data = iris)
-  (summary_m1 <- summary(m1))
-  
-
-  
-  ggplot(fly_dat1,aes(x=as.numeric(treatment),y=lifespan_box,colour=pop))+
-    theme_bw()+theme(panel.border = element_rect(linetype = "solid", colour = "black",size=1))+
-    scale_colour_hue(l=60)+
-    theme(text=element_text(family="Times",size=20))+
-    xlab("Male experience")+
-    ylab("Lifespan")+geom_point(shape=1)+
-    geom_smooth(method="lm",alpha=0.1)
